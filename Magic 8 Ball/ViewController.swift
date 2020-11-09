d6a4d89b94231e0a0b3b56ab424f7a0dff4ce3ed
The-Natalie <natalie.diana.hall@gmail.com>
2020-11-09 00:31:09 -0500

    My changes

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -6,11 +6,16 @@
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
-import UIKit
+import UIKit;
 
 class ViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!;
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")];
+        imageView.image = ballArray[Int.random(in: 0...4)];
+
+    }
 
 
 
